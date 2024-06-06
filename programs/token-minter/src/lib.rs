a10851c2e5d6e83c62ecd4f107207e3eeb79730d
Strike <strikeihaveadream@gmail.com>
2024-06-06 16:15:26 +0300

    fix error when user buy others token

--- programs/token-minter/src/lib.rs
+++ programs/token-minter/src/lib.rs
@@ -1,7 +1,5 @@
 // 1. Import dependencies
 use anchor_lang::prelude::*;
-use solana_program::program::invoke;
-use solana_program::system_instruction::transfer;
 use anchor_spl::{
     associated_token::AssociatedToken,
     metadata::{
@@ -10,6 +8,8 @@ use anchor_spl::{
     },
     token::{self, mint_to, Mint, MintTo, Token, TokenAccount},
 };
+use solana_program::program::invoke;
+use solana_program::system_instruction::transfer;
 
 // 2. Declare Program ID (SolPG will automatically update this when you deploy)
 declare_id!("2zP1cXE8o6dBDuNwfxoHgydP7ufn5sBibShiuv86RJ5b");
@@ -44,9 +44,15 @@ mod token_minter {
         Ok(())
     }
 
-    pub fn create_token(ctx: Context<InitToken>, metadata: InitTokenParams, amount: u64) -> Result<()> {
-
-        require!(amount < ctx.accounts.global_info.initial_amount, CustomError::InvalidInitialValue);
+    pub fn create_token(
+        ctx: Context<InitToken>,
+        metadata: InitTokenParams,
+        amount: u64,
+    ) -> Result<()> {
+        require!(
+            amount < ctx.accounts.global_info.initial_amount,
+            CustomError::InvalidInitialValue
+        );
         ctx.accounts.token_pools.sol_reserve = ctx.accounts.global_info.initial_amount;
         ctx.accounts.token_pools.token_reserve = ctx.accounts.global_info.total_supply;
 
@@ -55,7 +61,7 @@ mod token_minter {
         let signer = [&seeds[..]];
 
         let token_data: DataV2 = DataV2 {
-        name: metadata.name,
+            name: metadata.name,
             symbol: metadata.symbol,
             uri: metadata.uri,
             seller_fee_basis_points: 0,
@@ -97,9 +103,11 @@ mod token_minter {
 
         let buy_fee: u64 = calculate_fee(amount, ctx.accounts.global_info.fee_percent);
         let effective_sol: u64 = amount - buy_fee;
-        let token_price: u64 = get_price(ctx.accounts.token_pools.sol_reserve, ctx.accounts.token_pools.token_reserve);
+        let token_price: u64 = get_price(
+            ctx.accounts.token_pools.sol_reserve,
+            ctx.accounts.token_pools.token_reserve,
+        );
         let token_amount: u64 = (effective_sol * 1e9 as u64) / token_price;
-
 
         msg!("Token mint created successfully.");
 
@@ -129,8 +137,8 @@ mod token_minter {
             token_amount,
         )?;
 
-        
-        ctx.accounts.token_pools.token_reserve = ctx.accounts.global_info.total_supply - token_amount;
+        ctx.accounts.token_pools.token_reserve =
+            ctx.accounts.global_info.total_supply - token_amount;
         ctx.accounts.token_pools.launched = 0;
         ctx.accounts.global_info.token_count += 1;
 
@@ -140,7 +148,10 @@ mod token_minter {
     pub fn buy_token(ctx: Context<BuyToken>, amount: u64) -> Result<()> {
         let buy_fee: u64 = calculate_fee(amount, ctx.accounts.global_info.fee_percent);
         let effective_sol: u64 = amount - buy_fee;
-        let token_price: u64 = get_price(ctx.accounts.token_pools.sol_reserve, ctx.accounts.token_pools.token_reserve);
+        let token_price: u64 = get_price(
+            ctx.accounts.token_pools.sol_reserve,
+            ctx.accounts.token_pools.token_reserve,
+        );
         let token_amount: u64 = (effective_sol * 1e9 as u64) / token_price;
         // Transfer SOL from buyer to contract account
         let transfer_instruction = transfer(
@@ -157,7 +168,10 @@ mod token_minter {
             ],
         )?;
 
-        require!(ctx.accounts.token_pools.token_reserve > token_amount, CustomError::InvalidTokenAmount);
+        require!(
+            ctx.accounts.token_pools.token_reserve > token_amount,
+            CustomError::InvalidTokenAmount
+        );
 
         let binding = ctx.accounts.mint.key();
         let seeds = &[binding.as_ref(), &[ctx.bumps.token_vault]];
@@ -168,7 +182,7 @@ mod token_minter {
             token::Transfer {
                 from: ctx.accounts.token_vault.to_account_info(),
                 to: ctx.accounts.buyer_token_account.to_account_info(),
-                authority: ctx.accounts.token_vault.to_account_info()
+                authority: ctx.accounts.token_vault.to_account_info(),
             },
             signer_seeds,
         );
@@ -183,24 +197,38 @@ mod token_minter {
     pub fn sell_token(ctx: Context<SellToken>, token_amount: u64) -> Result<()> {
         let sell_fee: u64 = calculate_fee(token_amount, ctx.accounts.global_info.fee_percent);
         let effective_token_amount: u64 = token_amount - sell_fee;
-        let token_price: u64 = get_price(ctx.accounts.token_pools.sol_reserve, ctx.accounts.token_pools.token_reserve);
+        let token_price: u64 = get_price(
+            ctx.accounts.token_pools.sol_reserve,
+            ctx.accounts.token_pools.token_reserve,
+        );
         let sol_amount: u64 = (effective_token_amount * token_price) / 1e9 as u64;
         // Transfer tokens from seller to contract account
-        
+
         let cpi_context = CpiContext::new(
             ctx.accounts.token_program.to_account_info(),
             token::Transfer {
                 from: ctx.accounts.seller_token_account.to_account_info(),
                 to: ctx.accounts.token_vault.to_account_info(),
-                authority: ctx.accounts.seller.to_account_info()
-            }
+                authority: ctx.accounts.seller.to_account_info(),
+            },
         );
         token::transfer(cpi_context, token_amount)?;
 
-        require!(ctx.accounts.token_pools.sol_reserve > sol_amount, CustomError::InvalidSolAmount);
+        require!(
+            ctx.accounts.token_pools.sol_reserve > sol_amount,
+            CustomError::InvalidSolAmount
+        );
         // Transfer SOL from contract account to seller
-        **ctx.accounts.escrow_account.to_account_info().try_borrow_mut_lamports()? -= sol_amount;
-        **ctx.accounts.seller.to_account_info().try_borrow_mut_lamports()? += sol_amount;
+        **ctx
+            .accounts
+            .escrow_account
+            .to_account_info()
+            .try_borrow_mut_lamports()? -= sol_amount;
+        **ctx
+            .accounts
+            .seller
+            .to_account_info()
+            .try_borrow_mut_lamports()? += sol_amount;
 
         ctx.accounts.token_pools.sol_reserve -= sol_amount;
         ctx.accounts.token_pools.token_reserve += token_amount;
@@ -209,19 +237,13 @@ mod token_minter {
     }
 
     pub fn add_liquidity(ctx: Context<AddLiquidity>, sol_price: u64) -> Result<()> {
-        let init_coin_amount = ctx.accounts.global_info.target_lp_amount * 1e9 as u64 / sol_price / 1000;
-        let init_pc_amount = ctx.accounts.token_pools.token_reserve / ctx.accounts.token_pools.sol_reserve * init_coin_amount; 
+        let init_coin_amount =
+            ctx.accounts.global_info.target_lp_amount * 1e9 as u64 * 1000 / sol_price;
+        let init_pc_amount = ctx.accounts.token_pools.token_reserve
+            / ctx.accounts.token_pools.sol_reserve * init_coin_amount;
 
-        **ctx.accounts.escrow_account.try_borrow_mut_lamports()? -= init_coin_amount;
-        **ctx.accounts.user_token_pc.try_borrow_mut_lamports()? += init_coin_amount;
-
-        token::sync_native(CpiContext::new(
-                ctx.accounts.token_program.to_account_info(),
-                token::SyncNative {
-                    account: ctx.accounts.user_token_pc.to_account_info()
-                }
-            ))?;
-    
+        let pda_account = ctx.accounts.escrow_account.to_account_info();
+        let send_to_account = ctx.accounts.user_token_pc.to_account_info();
 
         let binding = ctx.accounts.token_vault.mint.key();
         let seeds = &[binding.as_ref(), &[ctx.bumps.token_vault]];
@@ -232,17 +254,31 @@ mod token_minter {
             token::Transfer {
                 from: ctx.accounts.token_vault.to_account_info(),
                 to: ctx.accounts.user_token_coin.to_account_info(),
-                authority: ctx.accounts.token_vault.to_account_info()
+                authority: ctx.accounts.token_vault.to_account_info(),
             },
             signer_seeds,
         );
+        
         token::transfer(transfer_ctx, init_pc_amount)?;
+
+        **pda_account.try_borrow_mut_lamports()? -= init_coin_amount;
+        **send_to_account.try_borrow_mut_lamports()? += init_coin_amount;
+
+        // token::sync_native(CpiContext::new(
+        //     ctx.accounts.token_program.to_account_info(),
+        //     token::SyncNative {
+        //         account: ctx.accounts.user_token_pc.to_account_info(),
+        //     },
+        // ))?;
 
         Ok(())
     }
 
     pub fn withdraw_balance(ctx: Context<WithdrawBalance>, amount: u64) -> Result<()> {
-        require!(ctx.accounts.global_info.owner == ctx.accounts.admin.key(), CustomError::NotOwner);
+        require!(
+            ctx.accounts.global_info.owner == ctx.accounts.admin.key(),
+            CustomError::NotOwner
+        );
         let pool_amount = **ctx.accounts.escrow_account.lamports.borrow();
         require!(pool_amount >= amount, CustomError::InvalidSolAmount);
         **ctx.accounts.escrow_account.try_borrow_mut_lamports()? -= amount;
@@ -253,31 +289,46 @@ mod token_minter {
     }
 
     pub fn set_fee_percent(ctx: Context<CommonCtx>, fee_percent: u32) -> Result<()> {
-        require!(ctx.accounts.global_info.owner == ctx.accounts.admin.key(), CustomError::NotOwner);
+        require!(
+            ctx.accounts.global_info.owner == ctx.accounts.admin.key(),
+            CustomError::NotOwner
+        );
         ctx.accounts.global_info.fee_percent = fee_percent;
         Ok(())
     }
 
     pub fn set_target_market_cap(ctx: Context<CommonCtx>, target_market_cap: u64) -> Result<()> {
-        require!(ctx.accounts.global_info.owner == ctx.accounts.admin.key(), CustomError::NotOwner);
+        require!(
+            ctx.accounts.global_info.owner == ctx.accounts.admin.key(),
+            CustomError::NotOwner
+        );
         ctx.accounts.global_info.target_market_cap = target_market_cap;
         Ok(())
     }
 
     pub fn set_target_lp_amount(ctx: Context<CommonCtx>, target_lp_amount: u64) -> Result<()> {
-        require!(ctx.accounts.global_info.owner == ctx.accounts.admin.key(), CustomError::NotOwner);
+        require!(
+            ctx.accounts.global_info.owner == ctx.accounts.admin.key(),
+            CustomError::NotOwner
+        );
         ctx.accounts.global_info.target_lp_amount = target_lp_amount;
         Ok(())
     }
 
     pub fn set_total_supply(ctx: Context<CommonCtx>, total_supply: u64) -> Result<()> {
-        require!(ctx.accounts.global_info.owner == ctx.accounts.admin.key(), CustomError::NotOwner);
+        require!(
+            ctx.accounts.global_info.owner == ctx.accounts.admin.key(),
+            CustomError::NotOwner
+        );
         ctx.accounts.global_info.total_supply = total_supply;
         Ok(())
     }
 
     pub fn set_initial_amount(ctx: Context<CommonCtx>, initial_amount: u64) -> Result<()> {
-        require!(ctx.accounts.global_info.owner == ctx.accounts.admin.key(), CustomError::NotOwner);
+        require!(
+            ctx.accounts.global_info.owner == ctx.accounts.admin.key(),
+            CustomError::NotOwner
+        );
         ctx.accounts.global_info.initial_amount = initial_amount;
         Ok(())
     }
@@ -396,17 +447,23 @@ pub struct BuyToken<'info> {
         bump,
     )]
     pub token_pools: Box<Account<'info, TokenPools>>,
-    #[account(mut)]
+    #[account(
+        init_if_needed,
+        payer = buyer,
+        associated_token::mint = mint,
+        associated_token::authority = buyer,
+    )]
     pub buyer_token_account: Box<Account<'info, TokenAccount>>,
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 #[derive(Accounts)]
 pub struct SellToken<'info> {
     #[account(mut)]
     pub seller: Signer<'info>,
-     #[account(mut)]
+    #[account(mut)]
     pub mint: Box<Account<'info, Mint>>,
     #[account(
         mut,
@@ -484,24 +541,19 @@ pub struct CommonCtx<'info> {
 
 #[derive(Accounts)]
 pub struct AddLiquidity<'info> {
+    #[account()]
+    pub mint: Box<Account<'info, Mint>>,
     /// CHECK: Safe. The user coin token
     #[account(
-        mut,
-        owner = token_program.key(),
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint,
+        associated_token::authority = payer,
     )]
-    pub user_token_coin: UncheckedAccount<'info>,
+    pub user_token_coin: Box<Account<'info, TokenAccount>>,
     /// CHECK: Safe. The user pc token
-    #[account(
-        mut,
-        owner = token_program.key(),
-    )]
+    #[account(mut)]
     pub user_token_pc: UncheckedAccount<'info>,
-    /// CHECK: Safe. The user lp token
-    #[account(mut)]
-    pub user_token_lp: UncheckedAccount<'info>,
-    
-    #[account(mut)]
-    pub mint: Box<Account<'info, Mint>>,
     #[account(
         mut,
         token::mint = mint,
@@ -512,21 +564,20 @@ pub struct AddLiquidity<'info> {
     pub token_vault: Box<Account<'info, TokenAccount>>,
     /// CHECK: This is vault account.
     #[account(
-        mut
+        mut,
+        seeds = [ SOL_VAULT_SEED.as_bytes(), mint.key().as_ref() ],
+        bump,
     )]
     pub escrow_account: UncheckedAccount<'info>,
     #[account(mut)]
     pub payer: Signer<'info>,
-    #[account(
-        mut
-    )]
+    #[account(mut)]
     pub global_info: Box<Account<'info, GlobalInfo>>,
-    #[account(
-        mut
-    )]
+    #[account(mut)]
     pub token_pools: Box<Account<'info, TokenPools>>,
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 // 5. Define the init token params
@@ -547,14 +598,14 @@ pub struct GlobalInfo {
     pub initial_amount: u64,
     pub token_count: u32,
     pub liquidity_added: bool,
-    pub owner: Pubkey
+    pub owner: Pubkey,
 }
 
 #[account]
 pub struct TokenPools {
     pub sol_reserve: u64,
     pub token_reserve: u64,
-    pub launched: u8        // 0 -> false, 1 -> true
+    pub launched: u8, // 0 -> false, 1 -> true
 }
 
 #[error_code]
